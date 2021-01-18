1326b55eab72a194fedb1431d5b710d292b9f928
SaiPradeepti <saipradeepti14@gmail.com>
2021-01-18 19:34:07 +0530

    Basic Counter Using useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React, { useState, useReducer } from 'react'
+import React, { useReducer } from 'react'
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+  RESET: 'reset'
+}
+
+const reducer = (state,action) => {
+  switch(action.type){
+    case ACTIONS.INCREMENT:
+      return {count: state.count + 1}
+    case ACTIONS.DECREMENT:
+      return {count: state.count - 1}
+    case ACTIONS.RESET:
+      return {count: 0}
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [count,setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {count: 0})
   
-  const increment = () => setCount(prevCount => count+1);
-  const decrement = () => setCount(prevCount => count-1);
+  const increment = () => dispatch({ type: ACTIONS.INCREMENT });
+  const decrement = () => dispatch({ type: ACTIONS.DECREMENT });
+  const reset = () => dispatch({ type: ACTIONS.RESET });
   
   return (
     <>
       <div className="counterContainer">
         <div className="counter">
           <button onClick={decrement}>-</button>
-          <span className="value">{count}</span>
+          <span className="value">{state.count}</span>
           <button onClick={increment}>+</button>
         </div>
         <div className="resetConainer">
-          <button className="reset" onClick={()=>setCount(0)}>Reset</button>
+          <button className="reset" onClick={reset}>Reset</button>
         </div>      
       </div>
     </>
