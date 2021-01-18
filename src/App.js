a6610574c90ba8fb6b5998136dde6f6175f00a74
SaiPradeepti <saipradeepti14@gmail.com>
2021-01-18 19:47:46 +0530

    Modifying Files Setup

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import React, { useReducer } from 'react'
+import React from 'react'
+import BasicCounter from "./tutorial/1.Basic-Counter";
 
-const ACTIONS = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement',
-  RESET: 'reset'
-}
-
-const reducer = (state,action) => {
-  switch(action.type){
-    case ACTIONS.INCREMENT:
-      return {count: state.count + 1}
-    case ACTIONS.DECREMENT:
-      return {count: state.count - 1}
-    case ACTIONS.RESET:
-      return {count: 0}
-    default:
-      return state;
-  }
-}
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {count: 0})
-  
-  const increment = () => dispatch({ type: ACTIONS.INCREMENT });
-  const decrement = () => dispatch({ type: ACTIONS.DECREMENT });
-  const reset = () => dispatch({ type: ACTIONS.RESET });
-  
   return (
-    <>
-      <div className="counterContainer">
-        <div className="counter">
-          <button onClick={decrement}>-</button>
-          <span className="value">{state.count}</span>
-          <button onClick={increment}>+</button>
-        </div>
-        <div className="resetConainer">
-          <button className="reset" onClick={reset}>Reset</button>
-        </div>      
-      </div>
-    </>
+    <BasicCounter />
   );
 }
 
