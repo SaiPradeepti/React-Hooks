356e78ed542cdfa8d1e3caf545db50ac136c1c38
SaiPradeepti <saipradeepti14@gmail.com>
2021-01-20 12:04:30 +0530

    Using payload in useReducer Hook in Basic-Counter.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BasicCounter from "./tutorial/1.Basic-Counter";
+import ToDo from "./tutorial/2.todo";
 
 
 function App() {
   return (
     <BasicCounter />
+    // <ToDo />
   );
 }
 
